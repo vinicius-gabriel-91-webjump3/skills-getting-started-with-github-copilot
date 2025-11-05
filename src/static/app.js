71fbe03d3375c08ff295acf8a1717cbc2f550a82
vinicius-gabriel-91-webjump3 <vinicius.gabriel@webjump.ai>
2025-11-05 20:10:12 +0000

    Adiciona seção de participantes nas atividades e estilização correspondente

--- src/static/app.js
+++ src/static/app.js
@@ -20,11 +20,32 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const spotsLeft = details.max_participants - details.participants.length;
 
+        let participantsSection = '';
+        if (details.participants.length > 0) {
+          const participantsList = details.participants
+            .map(participant => `<li>${participant}</li>`)
+            .join('');
+          participantsSection = `
+            <div class="participants-list">
+              <h5>Current Participants:</h5>
+              <ul>${participantsList}</ul>
+            </div>
+          `;
+        } else {
+          participantsSection = `
+            <div class="participants-list">
+              <h5>Current Participants:</h5>
+              <p class="no-participants">No participants yet - be the first to sign up!</p>
+            </div>
+          `;
+        }
+
         activityCard.innerHTML = `
           <h4>${name}</h4>
           <p>${details.description}</p>
           <p><strong>Schedule:</strong> ${details.schedule}</p>
           <p><strong>Availability:</strong> ${spotsLeft} spots left</p>
+          ${participantsSection}
         `;
 
         activitiesList.appendChild(activityCard);
